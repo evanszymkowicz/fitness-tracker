0ad6a0575f05e7a871e43cf4d3fbe1f4c7a0384c
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-05-23 16:49:26 -0400

    frontend

--- partyinthebackend/server.js
+++ partyinthebackend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
@@ -9,7 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
---
+++ src/components/exerciseList/exercise-list.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>View workouts</p>
+      </div>
+    )
+  }
+}
